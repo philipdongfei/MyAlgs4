738aab4b8a24a1da59d1dc04947f09715df3306c
PhlipTung <philip.dongfei@gmail.com>
2018-12-18 21:47:35 +0800

    Shell sort

--- Chapter21_Code/Shell.java
+++ Chapter21_Code/Shell.java
@@ -8,10 +8,16 @@ public class Shell{
     public static void sort(Comparable[] a) {
         // Sort a[] into increasing order.
         int N = a.length;
-        for (int i = 1; i < N; i++)
-        { // Insert a[i] among a[i-1], a[i-2],a[i-3].....
-            for (int j = i; j > 0 && less(a[j],a[j-1]); j--)
-                exch(a, j, j-1);
+        int h = 1;
+        while (h < N/3) h = 3*h + 1; // 1,4,13,40,121,364,1093
+        while (h >= 1)
+        {  // h-sort the array.
+            for (int i = h; i < N; i++){
+                // Insert a[i] among a[i-h],a[i-2*h],a[i-3*h]....
+                for (int j = i; j >= h && less(a[j],a[j-h]); j -= h)
+                    exch(a,j,j-h);
+            }
+            h = h/3;
         }
     }
     
